ac09f9ea10878c47f48a01c45e5f7450adcd16b3
mshokalyuk <mshokalyuk@chegg.com>
2014-08-21 14:15:35 +0300

    added packaje json

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
 console.log('!');
+
+var express = require('express'),
+	http = require('http'), 
+	path = require('path'),
+	config = require('./config')(),
+	app = express(),
+	MongoClient = require('mongodb').MongoClient;
